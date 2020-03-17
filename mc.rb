5d03506eefb8fbda6a9c8453dbc624f50d0b3f20
Koen Vendrik <k.vendrik@gmail.com>
2020-03-17 19:51:34 -0400

    updates mc

--- mc.rb
+++ mc.rb
@@ -1,9 +1,9 @@
 class Mc < Formula
   desc "Memoizes the output of a given command."
   homepage "https://github.com/kvendrik/mc"
-  url "https://github.com/kvendrik/mc/archive/1.0.0-alpha.tar.gz"
-  sha256 "a0ec75962bd028034deb661f39868295906ff51f2cc6c6cead24ad2c2a36d1dc"
-  version "1.0.0-alpha"
+  url "https://github.com/kvendrik/mc/archive/1.0.0-alpha.1.tar.gz"
+  sha256 "018d54a886ab657b7118e54d3c3b608dc053728b26778ff9b7c0c27c1adaf630"
+  version "1.0.0-alpha.1"
 
   bottle :unneeded
 
